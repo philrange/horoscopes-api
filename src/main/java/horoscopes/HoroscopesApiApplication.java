d027ead46408804ce3e8df9ba08d90f324c8879e
Phil Range <philrange@gmail.com>
2024-10-30 10:51:44 +0000

    fix CORS thing

--- src/main/java/horoscopes/HoroscopesApiApplication.java
+++ src/main/java/horoscopes/HoroscopesApiApplication.java
@@ -2,6 +2,9 @@ package horoscopes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HoroscopesApiApplication {
@@ -10,6 +13,7 @@ public class HoroscopesApiApplication {
 		SpringApplication.run(HoroscopesApiApplication.class, args);
 	}
 
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
